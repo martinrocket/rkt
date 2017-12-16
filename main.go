d26c75ec3cdc06e2016783e6c7b041d18647a526
martinrocket <martin.ruble@gmx.com>
2017-12-16 16:40:04 -0700

    add html

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>RKTPOS, startin up....</h1><p>yep, here we go</p>")
+}
+
 func main() {
-	fmt.Printf("hello, world!\n")
+	fmt.Println("Starting RKT......")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
