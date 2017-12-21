8420fa889b0cc96f0df72c31185e2aec8adad507
martinrocket <martin.ruble@gmx.com>
2017-12-21 16:54:59 -0700

    add scanln

--- main.go
+++ main.go
@@ -5,12 +5,29 @@ import (
 	"net/http"
 )
 
+var myPage = `
+
+<h1>RKTPOS, startin up....</h1>
+<p>yep, here we go</p>
+<p>yep yep</p>
+
+`
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>RKTPOS, startin up....</h1><p>yep, here we go</p>")
+	fmt.Fprintf(w, myPage)
 }
 
 func main() {
 	fmt.Println("Starting RKT......")
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	fmt.Print("Select an option: ") // Prompt user to select from menu.
+	var selection []byte            // Variable to hold selection from user input
+	for {
+		http.HandleFunc("/", handler)
+		http.ListenAndServe(":8080", nil)
+		fmt.Scanln(&selection) // Scan user input and assign it to memory location for var selection
+		fmt.Println()
+		if string(selection) == "x" {
+			http.Shutdown()
+		}
+	}
 }
