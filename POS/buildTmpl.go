1fa719aa1d2971a79fdffda90c2371d712a0eca1
martinrocket <martin.ruble@gmx.com>
2018-02-17 08:18:08 -0700

    add contenet

--- POS/buildTmpl.go
+++ POS/buildTmpl.go
@@ -6,14 +6,18 @@ import (
 	"os"
 )
 
-type myStruct struct {
+// myStruct is an example struct to be written into a template.
+type MyButton struct {
 	Name      string
 	PositionX int
 	PositionY int
+	Action    string
 }
 
+// myData is an example of a string to be written into a template.
 const myData = "hello"
 
+// myFooter is an exmple of a string written into a template with template.HTML formatting.
 const myFooter template.HTML = `
 		<div><p>
 
@@ -29,21 +33,34 @@ const myFooter template.HTML = `
 `
 
 func main() {
-
-	myDataStruct := myStruct{
-		Name:      "Martin",
+	// myDataStruct uses myStuct to build some test data for the template "Index.html"
+	Button1 := MyButton{
+		Name:      "Pepsi",
 		PositionX: 100,
 		PositionY: 200,
+		Action:    "Sell Item",
 	}
 
+	Button2 := MyButton{
+		Name:      "Sprite",
+		PositionX: 300,
+		PositionY: 200,
+		Action:    "Sell Item",
+	}
+
+	ButtonGrp1 := []MyButton{Button1, Button2}
+
+	// t is a template of index.html
 	t, err := template.ParseFiles("index.html")
 	if err != nil {
 		log.Printf("Error parsing file %v: %v/n", t.Name(), err)
 	}
 
+	// f is a new file in the local directory called edit.html where the t template will be written to for persistance.
 	f, _ := os.Create("edit.html")
+
 	t.ExecuteTemplate(os.Stdout, "POSHeader", myData)
-	t.ExecuteTemplate(os.Stdout, "POSBody", myDataStruct)
+	t.ExecuteTemplate(os.Stdout, "POSBody", ButtonGrp1)
 	t.ExecuteTemplate(f, "POSFooter", myFooter)
 
 }
